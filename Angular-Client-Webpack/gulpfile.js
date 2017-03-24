bffe80e24d79016a9610d5df7924a8fe8051bb72
FabianGosebrink <fabian.gosebrink@hotmail.com>
2017-03-24 12:54:35 +0100

    got rid of electron gulp task -electron (1/2)

--- Angular-Client-Webpack/gulpfile.js
+++ Angular-Client-Webpack/gulpfile.js
@@ -5,12 +5,10 @@ var taskListing = require('gulp-task-listing');
 gulp.task('default', ['help']);
 gulp.task('help', taskListing.withFilters(/-/));
 
-require('./gulpTasks/electron');
 require('./gulpTasks/cordova');
 
 gulp.task('build:all', function (done) {
     runSeq(
-        'build:electron:prod',
         'build:apps',
         done);
 });
