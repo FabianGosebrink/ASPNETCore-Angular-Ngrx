cf82d73f702278f8189e87cf53847f6a34093295
FabianGosebrink <fabian.gosebrink@outlook.com>
2017-03-25 21:44:56 +0100

    fixing build

--- Angular-Client-Webpack/app/shared/models/foodItem.model.ts
+++ Angular-Client-Webpack/app/shared/models/foodItem.model.ts
@@ -1,6 +1,6 @@
 export class FoodItem {
-    constructor(public id: number,
-        public calories: number,
-        public name: string,
-        public created: Date) { }
+    public id: number;
+    public calories: number;
+    public name: string;
+    public created: Date;
 }
