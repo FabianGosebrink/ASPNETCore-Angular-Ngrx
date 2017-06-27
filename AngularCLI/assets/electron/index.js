8b49eab17fa4a820ca4bcd8b74d639968ce9851f
FabianGosebrink <fabian.gosebrink@outlook.com>
2017-06-27 10:47:47 +0200

    added X-platform to CLI

---
+++ AngularCLI/assets/electron/index.js
@@ -0,0 +1,54 @@
+const { app, BrowserWindow, globalShortcut, Menu } = require('electron');
+
+const path = require('path');
+const url = require('url');
+const cpuValues = require('./cpuValues');
+const trayIcon = require('./trayIcon');
+
+let mainWindow = null;
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('ready', function () {
+
+  mainWindow = new BrowserWindow({
+    width: 1024,
+    height: 768,
+  });
+
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+
+  mainWindow.on('closed', () => {
+    win = null;
+  });
+
+  globalShortcut.register('CmdOrCtrl+Shift+i', () => {
+    mainWindow.webContents.toggleDevTools();
+  });
+
+  trayIcon.buildTrayIcon(mainWindow);
+  startSendCpuValues();
+});
+
+app.on('window-all-closed', () => {
+  app.quit()
+})
+
+let startSendCpuValues = () => {
+  setInterval(() => {
+    cpuValues.getCPUUsage((percentage) => {
+      console.log("sending to ipc channel: " + percentage);
+      if (mainWindow) {
+        mainWindow.webContents.send('newCpuValue', (percentage * 100).toFixed(2));
+      }
+    });
+  }, 1000);
+}
---
+++ AngularCLI/assets/electron/trayIcon.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow, globalShortcut, Menu, Tray, Notification } = require('electron');
+const path = require('path');
+
+module.exports = {};
+
+let buildTrayIcon = (mainWindow) => {
+  let trayIconPath = path.join(__dirname, 'icon.ico');
+
+  tray = new Tray(trayIconPath);
+  tray.setToolTip('eMeal');
+
+  var contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Open application',
+      click: function () {
+        mainWindow.show();
+      }
+    },
+    {
+      label: 'Quit',
+      click: function () {
+        app.isQuiting = true;
+        app.quit();
+      }
+    }]);
+
+  tray.setContextMenu(contextMenu);
+};
+
+module.exports.buildTrayIcon = buildTrayIcon;
