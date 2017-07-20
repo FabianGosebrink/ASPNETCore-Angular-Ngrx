25e5e15f10ea53fa21bfe0d98f6b46abeae581a5
FabianGosebrink <fabian.gosebrink@outlook.com>
2017-07-20 07:20:17 +0200

    removed weird import

--- AngularCLI/src/app/core/services/httpWrapper.service.ts
+++ AngularCLI/src/app/core/services/httpWrapper.service.ts
@@ -1,4 +1,3 @@
-import * as console from 'console';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
