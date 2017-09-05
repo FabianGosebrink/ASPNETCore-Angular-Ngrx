eef791cba926f3834bdc6a17129b0428ad71d60b
FabianGosebrink <fabian.gosebrink@outlook.com>
2017-09-05 07:34:19 +0200

    updating to latest versions and fixing small bug

--- AngularCLI/src/app/food/components/foodForm/foodForm.component.ts
+++ AngularCLI/src/app/food/components/foodForm/foodForm.component.ts
@@ -3,24 +3,25 @@ import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from
 import { FoodItem } from './../../../shared/models/foodItem.model';
 
 @Component({
-    selector: 'app-food-form',
-    templateUrl: './foodForm.component.html'
+  selector: 'app-food-form',
+  templateUrl: './foodForm.component.html'
 })
 
 export class FoodFormComponent implements OnChanges {
 
-    types: string[] = ['Starter', 'Main', 'Dessert'];
-    @Input() foodItem: FoodItem;
-    @Output() foodUpdated = new EventEmitter<FoodItem>();
-    @Output() foodAdded = new EventEmitter<FoodItem>();
+  types: string[] = ['Starter', 'Main', 'Dessert'];
+  @Input() foodItem: FoodItem;
+  @Output() foodUpdated = new EventEmitter<FoodItem>();
+  @Output() foodAdded = new EventEmitter<FoodItem>();
 
-    currentFood: FoodItem = new FoodItem();
+  currentFood: FoodItem = new FoodItem();
 
-    addOrUpdateFood() {
-        this.foodItem.id ? this.foodUpdated.emit(this.currentFood) : this.foodAdded.emit(this.currentFood);
-    }
+  addOrUpdateFood() {
+    this.foodItem.id ? this.foodUpdated.emit(this.currentFood) : this.foodAdded.emit(this.currentFood);
+  }
 
-    public ngOnChanges(changes: SimpleChanges): void {
-        this.currentFood = Object.assign({}, changes.foodItem.currentValue);
-    }
+  public ngOnChanges(changes: SimpleChanges): void {
+    this.currentFood = Object.assign({}, changes.foodItem.currentValue);
+    alert(JSON.stringify(this.currentFood));
+  }
 }
--- AngularCLI/src/app/food/store/reducer/food.reducer.ts
+++ AngularCLI/src/app/food/store/reducer/food.reducer.ts
@@ -4,69 +4,75 @@ import { FoodItem } from '../../../shared/models/foodItem.model';
 import * as FoodActions from '../actions/food.actions';
 
 export interface FoodState {
-    foodItems: FoodItem[],
-    selectedItem: FoodItem
+  foodItems: FoodItem[],
+  selectedItem: FoodItem
 };
 
 export const initialState: FoodState = {
-    foodItems: [],
-    selectedItem: new FoodItem()
+  foodItems: [],
+  selectedItem: new FoodItem()
 };
 
 export function foodItemsReducer(state = initialState, action: Action): FoodState {
-    switch (action.type) {
+  switch (action.type) {
 
-        case FoodActions.ADD_FOOD_SUCCESS:
-            const addFoodAction = <FoodActions.AddFoodSuccessAction>action;
-            return {
-                ...state,
-                foodItems: state.foodItems.concat(addFoodAction.foodItem)
-            };
+    case FoodActions.ADD_FOOD_SUCCESS:
+      const addFoodAction = <FoodActions.AddFoodSuccessAction>action;
+      return Object.assign({}, state, {
+        foodItems: state.foodItems.concat(addFoodAction.foodItem),
+        selectedItem: new FoodItem()
+      });
 
-        case FoodActions.DELETE_FOOD_SUCCESS:
-            const deleteFoodAction = <FoodActions.DeleteFoodSuccessAction>action;
-            return {
-                ...state,
-                foodItems: state.foodItems.filter(item => item.id !== deleteFoodAction.foodItem.id)
-            };
+    case FoodActions.DELETE_FOOD_SUCCESS:
+      const deleteFoodAction = <FoodActions.DeleteFoodSuccessAction>action;
+      return Object.assign({}, state, {
+        foodItems: state.foodItems.filter(item => item.id !== deleteFoodAction.foodItem.id),
+        selectedItem: new FoodItem()
+      });
 
-        case FoodActions.UPDATE_FOOD_SUCCESS:
-            const updateFoodAction = <FoodActions.UpdateFoodSuccessAction>action;
-            return {
-                ...state,
-                foodItems: state.foodItems.map((item: FoodItem) => {
-                    return item.id === updateFoodAction.foodItem.id ? Object.assign({}, item, updateFoodAction.foodItem) : item;
-                }),
-                selectedItem: new FoodItem()
-            };
+    case FoodActions.UPDATE_FOOD_SUCCESS:
+      const updateFoodAction = <FoodActions.UpdateFoodSuccessAction>action;
+      return Object.assign({}, state, {
+        foodItems: state.foodItems.map((item: FoodItem) => {
+          return item.id === updateFoodAction.foodItem.id ? Object.assign({}, item, updateFoodAction.foodItem) : item;
+        }),
+        selectedItem: new FoodItem()
+      });
 
-        case FoodActions.LOAD_FOOD_SUCCESS:
-            const loadFoodAction = <FoodActions.LoadFoodSuccessAction>action;
-            return {
-                ...state,
-                foodItems: loadFoodAction.foodItems,
-                selectedItem: new FoodItem()
-            };
+    case FoodActions.LOAD_FOOD_SUCCESS:
+      const loadFoodAction = <FoodActions.LoadFoodSuccessAction>action;
+      return Object.assign({}, state, {
+        foodItems: loadFoodAction.foodItems,
+        selectedItem: new FoodItem()
+      });
 
-        default:
-            return state;
+    default:
+      return state;
 
-    }
+  }
 }
 
 export function selectedItemReducer(state = initialState, action: any): FoodState {
-    switch (action.type) {
+  switch (action.type) {
 
-        case FoodActions.SELECT_FOOD_SUCCESS:
-            const selectFoodAction = <FoodActions.SelectFoodSuccessAction>action;
-            return {
-                ...state,
-                foodItems: state.foodItems,
-                selectedItem: selectFoodAction.foodItem
-            };
+    case FoodActions.LOAD_FOOD:
+      const loadFoodAction = <FoodActions.LoadFoodAction>action;
 
-        default:
-            return state;
+      return Object.assign({}, state, {
+        foodItems: state.foodItems,
+        selectedItem: new FoodItem()
+      });
 
-    }
+    case FoodActions.SELECT_FOOD_SUCCESS:
+      const selectFoodAction = <FoodActions.SelectFoodSuccessAction>action;
+
+      return Object.assign({}, state, {
+        foodItems: state.foodItems,
+        selectedItem: selectFoodAction.foodItem
+      });
+
+    default:
+      return state;
+
+  }
 }
