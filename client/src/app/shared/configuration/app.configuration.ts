b8fd10b14a34a829a98a437a03c5a95cf1e231b8
FabianGosebrink <fabian.gosebrink@outlook.com>
2018-08-26 21:49:09 +0200

    fixing build

--- client/src/app/shared/configuration/app.configuration.ts
+++ client/src/app/shared/configuration/app.configuration.ts
@@ -1,23 +1,17 @@
 import { Injectable } from '@angular/core';
 import { ToasterConfig } from 'angular2-toaster';
-import { PlatformInformationProvider } from '../../core/services/platform-information.provider';
 
 @Injectable()
 export class Configuration {
   title = 'eMeal';
-  constructor(
-    private readonly platformInformationProvider: PlatformInformationProvider
-  ) {}
 
   authConfig = {
     CLIENT_ID: 'AngularFoodClient',
     GRANT_TYPE: 'password',
-    SCOPE: 'WebAPI',
+    SCOPE: 'WebAPI'
   };
 
   toasterConfig: ToasterConfig = new ToasterConfig({
-    positionClass: this.platformInformationProvider.isWeb
-      ? 'toast-bottom-right'
-      : 'toast-bottom-full-width',
+    positionClass: 'toast-bottom-right'
   });
 }
