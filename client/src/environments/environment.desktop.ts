6440b3aa8cda0dbdc1b5f0d749752d9d709caf85
FabianGosebrink <fabian.gosebrink@outlook.com>
2019-06-18 09:05:42 +0200

    changing api endpoint

--- client/src/environments/environment.desktop.ts
+++ client/src/environments/environment.desktop.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   mobile: false,
   desktop: true,
-  server: 'https://food-api-core.azurewebsites.net/',
+  server: 'https://conference-xplatform-server.azurewebsites.net/',
   apiUrl: 'api/',
 };
--- client/src/environments/environment.mobile.ts
+++ client/src/environments/environment.mobile.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   mobile: true,
   desktop: false,
-  server: 'https://food-api-core.azurewebsites.net/',
+  server: 'https://conference-xplatform-server.azurewebsites.net/',
   apiUrl: 'api/',
 };
